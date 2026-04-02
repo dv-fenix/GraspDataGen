aaaf08a27b7cc679955b009800376be394ecde0c
Divyam Goel <divyamgoel3@gmail.com>
2026-04-02 13:18:26 -0400

    :whale: [build] Update docker file and build scripts

--- docker/build.sh
+++ docker/build.sh
@@ -1,2 +1,2 @@
 VER=1.0
-docker build -f docker/Dockerfile --progress=plain . --network=host -t graspdatagen:$VER -t graspdatagen:latest
+docker build --network=host -f docker/Dockerfile -t graspdatagen:$VER -t graspdatagen:latest .
